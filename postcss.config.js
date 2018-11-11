b91a1a93305145b3be07932153c51a9060a45a24
Dorian Henry <dorian.henry.prof@gmail.com>
2018-11-11 16:00:36 +0100

    base with webpack

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+module.exports = {
+    plugins: {
+        // include whatever plugins you want
+        // but make sure you install these via yarn or npm!
+
+        // add browserslist config to package.json (see below)
+        autoprefixer: {}
+    }
+}
